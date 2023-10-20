a2335ab1804ac2303454dbe82ee5a898b6feabc9
MrBaykal <ebaykal2266@gmail.com>
2023-10-20 09:00:46 +0200

    "first upload"

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "MrBaykal"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "my_key"
+}
+
+variable "hosted-zone" {
+  default = "emrebaykal.click"
+}
