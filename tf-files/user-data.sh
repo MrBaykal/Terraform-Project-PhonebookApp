f681b2f8219adb959818707d06cc0c6b2f67eb77
MrBaykal <112976827+MrBaykal@users.noreply.github.com>
2024-06-11 11:31:12 +0200

    Update user-data.sh

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -6,5 +6,5 @@ pip3 install flask_mysql
 dnf install git -y
 TOKEN=${user-data-git-token}
 USER=${user-data-git-name}
-cd /home/ec2-user && git clone https://$TOKEN@github.com/$USER/phonebook.git
-python3 /home/ec2-user/phonebook/phonebook-app.py
+cd /home/ec2-user && git clone https://$TOKEN@github.com/$USER/project-terraform-phonebook-app.git
+python3 /home/ec2-user/project-terraform-phonebook-app/phonebook-app.py
